0c9b83d8191735b3cba16fc6ea47917f4bf27080
MaViWurd <marvelousegbe@gmail.com>
2022-06-29 14:23:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,15 @@
-{ #include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
-return (1);
-} }
+	return (1);
+}
