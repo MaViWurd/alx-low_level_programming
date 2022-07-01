26333fb64441b50446133a5faf557caf3dda5c69
MaViWurd <marvelousegbe@gmail.com>
2022-07-01 08:58:04 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,9 +12,8 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	for (letter = 'A'; letter <= 'z'; letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
-
 	putchar('\n');
 
 	return (0);
