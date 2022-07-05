ade89702c1394888940462bcc9756eaa7115e062
MaViWurd <marvelousegbe@gmail.com>
2022-07-05 15:50:51 -0700

    *

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,56 +1,44 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table up to the integer input
- * @n: integer input
- *
- * Return: void
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
-
 void print_times_table(int n)
 {
-	int a, b, c, d, e, f;
+	int num, mult, prod;
 
-	if (n >= 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <=  n; a++)
+		for (num = 0; num <= n; num++)
 		{
-			for (b = 0; b <= n; b++)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				c = a * b;
-				d = c / 100;
-				e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				else if (c < 10)
-				{
-					_putchar(' '); 
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
 					_putchar(' ');
-					_putchar('0' + f);
-				}
-				else if (c < 100)
-				{
+				if (prod <= 9)
 					_putchar(' ');
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				else
+
+				if (prod >= 100)
 				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				if (b < n)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
+					_putchar((prod / 10) + '0');
 				}
-				else
-					_putchar('\n');
+				_putchar((prod % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+
 /**
- * main - Entry point
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
 
 	return (0);
 }
