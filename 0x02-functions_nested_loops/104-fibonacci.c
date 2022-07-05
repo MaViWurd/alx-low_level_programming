073e86759e0d5c97efa4dcd89cb88bbf1769ba12
MaViWurd <marvelousegbe@gmail.com>
2022-07-05 14:52:31 -0700

    *

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,15 @@
+utable File  13 lines (12 sloc)  158 Bytes
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	return (0);
+}
