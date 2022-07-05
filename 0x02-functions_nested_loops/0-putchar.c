95c7aad20eae07ab41b7ab812cbd7d7926478f91
MaViWurd <marvelousegbe@gmail.com>
2022-07-05 10:16:19 -0700

    debugged the hell

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("e");
-	_putchar("r");
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('e');
+	_putchar('r');
+	_putchar('\n');
 
 	return 0;
 }
