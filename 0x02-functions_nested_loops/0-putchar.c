485462ac029767e4de1ee7ecc4adb2b63f07ca60
MaViWurd <marvelousegbe@gmail.com>
2022-07-05 07:00:02 -0700

    created a source code that prints _putchar and enter the next line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+**/
+*Author: EMarvel
+*
+* Date: 5th July 2022
+*
+* Return (0)
+*/
+
+int main()
+{
+	printf("_putchar\n");
+
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+created a 0x02-functions_nested_loops directory for my next project
