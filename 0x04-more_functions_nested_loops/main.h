dbc7cdf985b01c20bf38cb9da23fb9434e6ac2c1
MaViWurd <marvelousegbe@gmail.com>
2022-07-07 10:25:59 -0700

    task number 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
+
+#endif /* MAINN_H */
