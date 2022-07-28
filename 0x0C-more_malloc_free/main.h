be88f08a0c1214d0c57fe3b86b636ccc8320070c
MaViWurd <marvelousegbe17@gmail.com>
2022-07-28 09:28:32 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -12,9 +13,10 @@ int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-unsigned int *initDigitArray(size_t size);
-void stringIntMultiply(unsigned int *prod_digits, char *n1_digits,
-		char *n2_digits, size_t n1_len, size_t n2_len);
-int stringIsPosInt(char *s);
-void error(int status);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 #endif /*MAIN_H*/
