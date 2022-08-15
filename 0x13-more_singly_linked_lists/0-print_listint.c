8a9fc082eec8e0b03ade03304d6d2d90535824a4
MaViWurd <marvelousegbe17@gmail.com>
2022-08-15 11:47:22 -0700

    lists.h

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-const listint_t *cursor = h;
-size_t count = 0;
+	const listint_t *cursor = h;
+	size_t count = 0;
 
-while (cursor != NULL)
-{
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
-}
-return (count);
+	while (cursor != NULL)
+	{
+		printf("%d\n", cursor->n);
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
 }
